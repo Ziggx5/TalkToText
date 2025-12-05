bc7e9706cb35380ff428194ad0674944c528d886
Maj Sedonja <maj.sedonja5@gmail.com>
2025-12-06 00:30:34 +0100

    Starting files

---
+++ modules/ui.py
@@ -0,0 +1,7 @@
+from customtkinter import *
+
+def start_ui():
+    app = CTk()
+    app.geometry("700x500")
+    app.title("TalkToText")
+    app.mainloop()
---
+++ talktotext.py
@@ -0,0 +1,7 @@
+from modules.ui import *
+
+def main():
+    start_ui()
+
+if __name__ == "__main__":
+    main()
