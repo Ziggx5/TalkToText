07869562c93ea5beb56befba46a368e3c01f0750
Maj Sedonja <maj.sedonja5@gmail.com>
2025-12-08 01:17:12 +0100

    Working stop button, constant listening after start button click

--- modules/speech_recognition.py
+++ modules/speech_recognition.py
@@ -4,17 +4,24 @@ import threading
 recogniser = sr.Recognizer()
 
 def start_listening(text_box):
+    global listening
+    listening = True
     thread = threading.Thread(target = recognition_handler, args = (text_box,))
     thread.start()
 
 def recognition_handler(text_box):
     text_box.delete("0.0", "end")
     with sr.Microphone() as source:
-        try:
-            audio = recogniser.listen(source, timeout = 3)
-            text = recogniser.recognize_google(audio)
-            text_box.insert("end", text)
-        except sr.WaitTimeoutError:
-            text_box.insert("end", "No input")
-        except Exception as e:
-            text_box.insert("end", e)
+        while listening:
+            try:
+                audio = recogniser.listen(source)
+                text = recogniser.recognize_google(audio)
+                text_box.insert("end", text)
+            except sr.WaitTimeoutError:
+                text_box.insert("end", "No input")
+            except Exception as e:
+                text_box.insert("end", e)
+
+def stop_listening():
+    global listening
+    listening = False
