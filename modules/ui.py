850afad9b2dbb96f231449e80497c58e840e0b38
Maj Sedonja <maj.sedonja5@gmail.com>
2025-12-08 23:29:07 +0100

    Better UI, added clear button

--- modules/ui.py
+++ modules/ui.py
@@ -6,18 +6,21 @@ import os
 def start_ui():
     app = CTk()
     images = image_loader_handler()
-    app.geometry("400x400")
+    app.geometry("500x400")
     app.title("TalkToText")
     app.resizable(False, False)
     app.configure(fg_color = "#323232")
 
-    text_box = CTkTextbox(app, width = 300, height = 200)
-    text_box.place(x = 10, y = 50)
+    text_box = CTkTextbox(app, width = 300, height = 380, font = ("TkTextFont", 15))
+    text_box.place(x = 10, y = 10)
     
-    start_button = CTkButton(app, text = "Start", fg_color = "#3d3d3d", corner_radius = 5, image = images["start.png"], compound = "left", command = lambda: start_listening(text_box, start_button))
-    start_button.place(x = 10, y = 10)
+    start_button = CTkButton(app, text = "Start", fg_color = "#3d3d3d", corner_radius = 5, image = images["start.png"], compound = "left", width = 150, hover_color= "gray", command = lambda: start_listening(text_box, start_button))
+    start_button.place(x = 330, y = 10)
 
-    stop_button = CTkButton(app, text = "Stop", fg_color = "#3d3d3d", corner_radius = 5, image = images["stop.png"], compound = "left", command = lambda: stop_listening(start_button))
-    stop_button.place(x = 160, y = 10)
+    stop_button = CTkButton(app, text = "Stop", fg_color = "#3d3d3d", corner_radius = 5, image = images["stop.png"], compound = "left", width = 150, hover_color= "gray", command = lambda: stop_listening(start_button))
+    stop_button.place(x = 330, y = 50)
+
+    clear_button = CTkButton(app, text = "Clear", fg_color = "#3d3d3d", corner_radius = 5, image = images["clear.png"], compound = "left", width = 150, hover_color= "gray", command = lambda: text_box.delete("0.0", "end"))
+    clear_button.place(x = 330, y = 90)
 
     app.mainloop()
