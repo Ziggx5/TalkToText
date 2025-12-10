aa7acb3d6de741e055d46a56cd910d50407a53ae
Ziggx5 <maj.sedonja@gmail.com>
2025-12-10 13:49:53 +0100

    Added vosk for converting speech to text, minor code fixes

---
+++ modules/file_loader.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from PIL import *
+from customtkinter import *
+
+def get_parent_path():
+    if getattr(sys, "frozen", False):
+        base_path = os.path.dirname(sys.executable)
+    else:
+        base_path = os.path.dirname(os.path.abspath(__file__))
+    
+    parent_path = os.path.dirname(base_path)
+
+    return parent_path
+    
+def image_loader_handler():
+    parent_path = get_parent_path()
+    images_path = os.path.join(parent_path, "images")
+    images = {}
+    
+    for file_name in os.listdir(images_path):
+        file_path = os.path.join(images_path, file_name)
+        open_image = Image.open(file_path)
+        images[file_name] = CTkImage(light_image = open_image, dark_image = open_image, size = (15, 15))
+    
+    return images
