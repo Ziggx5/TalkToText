1fd4185fd800d23be8ec7dc40bf21750995e8189
Ziggx5 <maj.sedonja@gmail.com>
2025-12-10 01:12:43 +0100

    Added microphone strength visualisation bar

--- modules/speech_recognition.py
+++ modules/speech_recognition.py
@@ -5,20 +5,24 @@ from customtkinter import *
 
 recogniser = sr.Recognizer()
 
-def start_listening(text_box, start_button):
+def start_listening(text_box, start_button, sound_bar):
     global listening
     listening = True
-    thread = threading.Thread(target = recognition_handler, args = (text_box, start_button,))
+    thread = threading.Thread(target = recognition_handler, args = (text_box, start_button, sound_bar,))
     thread.start()
 
-def recognition_handler(text_box, start_button):
+def recognition_handler(text_box, start_button, sound_bar):
     start_button.configure(state = "disabled")
     text_box.delete("0.0", "end")
     last_text = ""
+
     with sr.Microphone() as source:
         while listening:
             try:
                 audio = recogniser.listen(source)
+                volume_strength = recogniser.energy_threshold * recogniser.dynamic_energy_ratio
+                level = min(volume_strength / 5000, 1)
+                sound_bar.set(level)
                 text = recogniser.recognize_google(audio)
                 if text != last_text:
                     text_box.insert("end", f" {text}")
--- modules/ui.py
+++ modules/ui.py
@@ -7,21 +7,31 @@ import os
 def start_ui():
     app = CTk()
     images = image_loader_handler()
-    app.geometry("500x400")
+    app.geometry("550x400")
     app.title("TalkToText")
     app.resizable(False, False)
     app.configure(fg_color = "#323232")
 
-    text_box = CTkTextbox(app, width = 300, height = 380, font = ("TkTextFont", 15))
+    text_box = CTkTextbox(app, width = 350, height = 380, font = ("TkTextFont", 15))
     text_box.place(x = 10, y = 10)
-    
-    start_button = CTkButton(app, text = "Start", fg_color = "#3d3d3d", corner_radius = 5, image = images["start.png"], compound = "left", width = 150, hover_color= "gray", command = lambda: start_listening(text_box, start_button))
-    start_button.place(x = 330, y = 10)
 
-    stop_button = CTkButton(app, text = "Stop", fg_color = "#3d3d3d", corner_radius = 5, image = images["stop.png"], compound = "left", width = 150, hover_color= "gray", command = lambda: stop_listening(start_button))
-    stop_button.place(x = 330, y = 50)
+    sound_bar_label = CTkLabel(app, text = "Mic strength:")
+    sound_bar_label.place(x = 380, y = 340)
 
-    clear_button = CTkButton(app, text = "Copy", fg_color = "#3d3d3d", corner_radius = 5, image = images["copy.png"], compound = "left", width = 150, hover_color= "gray", command = lambda: copy_to_clipboard(app, text_box))
-    clear_button.place(x = 330, y = 90)
+    sound_bar = CTkProgressBar(app, width = 150, progress_color = "green")
+    sound_bar.place(x = 380, y = 370)
+    sound_bar.set(0)
+
+    start_button = CTkButton(app, text = "Start", fg_color = "#3d3d3d", corner_radius = 5, image = images["start.png"], compound = "left", width = 150, height = 30, font = ("TkTextFont", 15), hover_color= "gray", command = lambda: start_listening(text_box, start_button, sound_bar))
+    start_button.place(x = 380, y = 10)
+
+    stop_button = CTkButton(app, text = "Stop", fg_color = "#3d3d3d", corner_radius = 5, image = images["stop.png"], compound = "left", width = 150, height = 30, font = ("TkTextFont", 15), hover_color= "gray", command = lambda: stop_listening(start_button))
+    stop_button.place(x = 380, y = 50)
+
+    clear_button = CTkButton(app, text = "Copy", fg_color = "#3d3d3d", corner_radius = 5, image = images["copy.png"], compound = "left", width = 150, height = 30, font = ("TkTextFont", 15), hover_color= "gray", command = lambda: copy_to_clipboard(app, text_box))
+    clear_button.place(x = 380, y = 90)
+
+    reset_button = CTkButton(app, text = "Reset", fg_color = "#3d3d3d", corner_radius = 5, image = images["reset.png"], compound = "left", width = 150, height = 30, font = ("TkTextFont", 15), hover_color= "gray", command = lambda: text_box.delete("1.0", "end"))
+    reset_button.place(x = 380, y = 130)
 
     app.mainloop()
