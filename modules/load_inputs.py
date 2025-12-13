439e7846d3b4315a24433e6d5bdd1d4d4d64b63b
Ziggx5 <maj.sedonja@gmail.com>
2025-12-13 17:54:43 +0100

    Changed colors on some icons, using dictionary instead of two lists for inputs and indexs

--- modules/load_inputs.py
+++ modules/load_inputs.py
@@ -2,21 +2,16 @@ import pyaudio
 
 def load_all_inputs():
     mic = pyaudio.PyAudio()
-    devices = []
-    device_index = []
-    short_name = []
+    devices = {}
 
     for i in range (mic.get_device_count()):
         mic_info = mic.get_device_info_by_index(i)
 
         if mic_info["maxInputChannels"] >= 1:
             device_name = mic_info['name']
-            devices.append(device_name)
-            device_index.append(i)
+            devices[device_name] = i
 
-            if len(device_name) > 10:
-                short_name.append(f"{device_name[:10]}...")
-            else:
-                short_name.append(device_name)
+    return devices
 
-    return devices, device_index, short_name
+def input_return(selected_input):
+    return selected_input
--- modules/ui.py
+++ modules/ui.py
@@ -2,7 +2,7 @@ from customtkinter import *
 from modules.speech_recognition import start_listening, stop_listening
 from modules.file_loader import image_loader_handler, icon_image_loader
 from modules.copy_logic import copy_to_clipboard
-from modules.load_inputs import load_all_inputs
+from modules.load_inputs import load_all_inputs, input_return
 from PIL import *
 import os
 
@@ -10,8 +10,8 @@ def start_ui():
     app = CTk()
     images = image_loader_handler()
     icon_photo = icon_image_loader()
-    devices, device_index, short_name = load_all_inputs()
-
+    devices = load_all_inputs()
+    print(devices)
     app.geometry("550x430")
     app.title("TalkToText")
     app.resizable(False, False)
@@ -21,22 +21,25 @@ def start_ui():
     text_box = CTkTextbox(app, width = 350, height = 380, font = ("TkTextFont", 15), wrap = "word")
     text_box.place(x = 10, y = 10)
 
-    status_label = CTkLabel(app, text = "Stopped...", font = ("TkTextFont", 15))
+    status_label = CTkLabel(app, text = "Idle...", font = ("TkTextFont", 15))
     status_label.place(x = 10, y = 395)
 
-    start_button = CTkButton(app, text = "Start", fg_color = "#3d3d3d", corner_radius = 5, image = images["start.png"], compound = "left", width = 150, height = 30, font = ("TkTextFont", 15), hover_color= "gray", command = lambda: start_listening(text_box, start_button, status_label))
+    input_label = CTkLabel(app, text = "Input device:", font = ("TkTextFont", 15))
+    input_label.place(x = 380, y = 360)
+
+    start_button = CTkButton(app, text = "Start", fg_color = "#3d3d3d", corner_radius = 5, image = images["start.png"], compound = "left", width = 150, font = ("TkTextFont", 15), hover_color= "gray", command = lambda: start_listening(text_box, start_button, status_label))
     start_button.place(x = 380, y = 10)
 
-    stop_button = CTkButton(app, text = "Stop", fg_color = "#3d3d3d", corner_radius = 5, image = images["stop.png"], compound = "left", width = 150, height = 30, font = ("TkTextFont", 15), hover_color= "gray", command = lambda: stop_listening(start_button, status_label))
+    stop_button = CTkButton(app, text = "Stop", fg_color = "#3d3d3d", corner_radius = 5, image = images["stop.png"], compound = "left", width = 150, font = ("TkTextFont", 15), hover_color= "gray", command = lambda: stop_listening(start_button, status_label))
     stop_button.place(x = 380, y = 50)
 
-    clear_button = CTkButton(app, text = "Copy", fg_color = "#3d3d3d", corner_radius = 5, image = images["copy.png"], compound = "left", width = 150, height = 30, font = ("TkTextFont", 15), hover_color= "gray", command = lambda: copy_to_clipboard(app, text_box))
+    clear_button = CTkButton(app, text = "Copy", fg_color = "#3d3d3d", corner_radius = 5, image = images["copy.png"], compound = "left", width = 150, font = ("TkTextFont", 15), hover_color= "gray", command = lambda: copy_to_clipboard(app, text_box))
     clear_button.place(x = 380, y = 90)
 
-    reset_button = CTkButton(app, text = "Reset", fg_color = "#3d3d3d", corner_radius = 5, image = images["reset.png"], compound = "left", width = 150, height = 30, font = ("TkTextFont", 15), hover_color= "gray", command = lambda: text_box.delete("1.0", "end"))
+    reset_button = CTkButton(app, text = "Reset", fg_color = "#3d3d3d", corner_radius = 5, image = images["reset.png"], compound = "left", width = 150, font = ("TkTextFont", 15), hover_color= "gray", command = lambda: text_box.delete("1.0", "end"))
     reset_button.place(x = 380, y = 130)
 
-    select_microphone = CTkOptionMenu(app, fg_color = "#3d3d3d", corner_radius = 5, width = 150, height = 30, font = ("TkTextFont", 15), values = short_name)
-    select_microphone.place(x = 380, y = 200)
-
+    select_microphone = CTkOptionMenu(app, fg_color = "#3d3d3d", corner_radius = 5, width = 150, height = 30, button_color = "#3d3d3d", button_hover_color = "gray", dynamic_resizing = False, font = ("TkTextFont", 15), values = list(devices.keys()), command = lambda: input_return(select_microphone.get()))
+    select_microphone.place(x = 380, y = 390)
+    
     app.mainloop()
