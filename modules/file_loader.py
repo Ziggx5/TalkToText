cc8d04dc8edea21b33c87a436144efeb30269536
Ziggx5 <maj.sedonja@gmail.com>
2025-12-25 16:05:46 +0100

    Small fix in file loading

--- modules/file_loader.py
+++ modules/file_loader.py
@@ -4,18 +4,21 @@ from PIL import *
 from customtkinter import *
 
 def get_project_root():
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    
+    if getattr(sys, 'frozen', False):
+        return os.path.dirname(sys.executable)
+    else:
+        return os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
 def image_loader_handler():
     root = get_project_root()
     images_path = os.path.join(root, "images")
     images = {}
-    
+
     for file_name in os.listdir(images_path):
         file_path = os.path.join(images_path, file_name)
         open_image = Image.open(file_path)
         images[file_name] = CTkImage(light_image = open_image, dark_image = open_image, size = (15, 15))
-    
+
     return images
 
 def icon_image_loader():
@@ -27,7 +30,4 @@ def icon_image_loader():
 def save_file_loader():
     root = get_project_root()
     save_file_path = os.path.join(root, "saves")
-    save_file_check = os.path.exists(save_file_path)
-
-    if not save_file_check:
-        os.mkdir(save_file_path)
+    os.makedirs(save_file_path)
